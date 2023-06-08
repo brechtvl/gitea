e29ab5442231163979ae8d160453e8088aad0ed6
Brecht Van Lommel <brecht@blender.org>
2023-06-08 14:43:48 +0200

    Allow non-local users to be renamed

--- services/user/user.go
+++ services/user/user.go
@@ -31,12 +31,13 @@ import (
 // RenameUser renames a user
 func RenameUser(ctx context.Context, u *user_model.User, newUserName string) error {
 	// Non-local users are not allowed to change their username.
-	if !u.IsOrganization() && !u.IsLocal() {
-		return user_model.ErrUserIsNotLocal{
-			UID:  u.ID,
-			Name: u.Name,
-		}
-	}
+	// BLENDER: allow renaming local users.
+	//if !u.IsOrganization() && !u.IsLocal() {
+	//	return user_model.ErrUserIsNotLocal{
+	//		UID:  u.ID,
+	//		Name: u.Name,
+	//	}
+	//}
 
 	if newUserName == u.Name {
 		return user_model.ErrUsernameNotChanged{
